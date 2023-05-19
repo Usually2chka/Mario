100099576f15828eabff3d14eb7e4b4bf7af2a63
Usually2chka <127783284+Usually2chka@users.noreply.github.com>
2023-05-19 14:53:42 +0300

    Final point

---
+++ core/src/com/mygdx/game/Sprites/Enemies/MashinkaGolfBall.java
@@ -0,0 +1,30 @@
+package com.mygdx.game.Sprites.Enemies;
+
+import com.mygdx.game.Screens.PlayScreen;
+import com.mygdx.game.Sprites.Mario;
+
+public class MashinkaGolfBall extends Enemy{
+    public MashinkaGolfBall(PlayScreen screen, float x, float y) {
+        super(screen, x, y);
+    }
+
+    @Override
+    protected void defineEnemy() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void hitOnHead(Mario mario) {
+
+    }
+
+    @Override
+    public void hitByEnemy(Enemy enemy) {
+
+    }
+}
---
+++ core/src/com/mygdx/game/Sprites/Enemies/MashinkaTurtle.java
@@ -0,0 +1,4 @@
+package com.mygdx.game.Sprites.Enemies;
+
+public class MashinkaTurtle {
+}
